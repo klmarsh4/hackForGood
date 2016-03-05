33af16ce9694c6210c0fda6f28c415242b523403
Michael Vander Meiden <m.vandermeiden@umiami.edu>
2016-03-05 18:48:44 -0500

    Will return the URL of the all instagram posts with the hard coded tag

---
+++ js/searchTag.js
@@ -0,0 +1,46 @@
+var accessToken = getAccessToken();
+// The tag that we want
+var tagName = "bob";
+
+
+var recentTagMediaUrl = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + accessToken;
+
+$.ajax({
+    method: "GET",
+    url: recentTagMediaUrl,
+    dataType: "jsonp",
+    jsonp : "callback",
+    jsonpCallback: "jsonpcallback",
+    success: searchForTag,
+    error: function(){console.log("error");},
+    complete: function(){console.log("complete");}
+});
+
+
+function searchForTag(obj){
+	var dataCount=obj.data.length;
+	while (dataCount>0){
+		 var tagCount=obj.data[dataCount - 1].tags.length
+		while (tagCount>0){
+			if (obj.data[dataCount-1].tags[tagCount-1] == tagName){
+
+				console.log(obj.data[dataCount-1].link)
+			
+			}
+		tagCount=tagCount-1;
+		}
+		
+		dataCount = dataCount - 1;
+	}
+		
+	
+}
+
+/*
+function displayImg(obj){
+	//console.log(myJson);
+	//var obj = JSON.parse(myJson);
+	var imgUrl = obj.data[0].images.low_resolution.url;
+	window.location = imgUrl;
+}
+*/
