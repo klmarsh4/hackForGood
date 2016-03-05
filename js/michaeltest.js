71bc806319175d3c2f81e067edd2fa16224a95c2
Kevin Marsh <klmarsh4@gmail.com>
2016-03-05 16:55:43 -0500

    now querying user instead of public - fix due to sandbox mode

--- js/michaeltest.js
+++ js/michaeltest.js
@@ -1,9 +1,9 @@
 var accessToken = getAccessToken();
-
 // The tag that we want
-var tagName = "puppy";
+var tagName = "lunch";
 
-var recentTagMediaUrl = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent?access_token=" + accessToken ;
+//var recentTagMediaUrl = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent?access_token=" + accessToken ;
+var recentTagMediaUrl = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + accessToken;
 
 //var recentTagMedia = JSON.parse(text);
 
