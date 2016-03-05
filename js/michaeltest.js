0bbab9d766a2017ae7c5b4450b62700952b458f1
Michael Vander Meiden <m.vandermeiden@umiami.edu>
2016-03-05 13:35:04 -0500

    Added get display image to michaeltest and added michaeltest to index

---
+++ js/michaeltest.js
@@ -0,0 +1,35 @@
+var accessToken = getAccess();
+
+// The tag that we want
+var tagName = "puppy";
+
+var recentTagMediaUrl = "https://api.instagram.com/v1/tags/" + tagName "/media/recent?access_token=" + accessToken ;
+
+//var recentTagMedia = JSON.parse(text);
+
+
+httpGetAsync(recentTagMediaUrl, displayImg);
+
+function displayImg(myJson){
+	
+	var obj = parse(myJson);
+	var imgUrl = obj.data[0].images.low_resolution.url;
+	window.location = imgUrl;
+}
+
+
+
+
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+
