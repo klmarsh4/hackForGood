a308f833ae4f5b886bc3129805017bd473cb1749
Kevin Marsh <klmarsh4@gmail.com>
2016-03-05 12:38:58 -0500

    getAccess is now a function

--- js/getAccess.js
+++ js/getAccess.js
@@ -1,9 +1,11 @@
-var url = window.location.href;
-var tokenIndex = url.indexOf("#access_token");
-if(tokenIndex == -1){
-  window.location = "https://api.instagram.com/oauth/authorize/?client_id=63e5ba506c314fbe856734e0b8421191&redirect_uri=" +url+ "REDIRECT-URI&response_type=token";
+function getAccessToken(){
+  var url = window.location.href;
+  var tokenIndex = url.indexOf("#access_token");
+  if(tokenIndex == -1){
+    window.location = "https://api.instagram.com/oauth/authorize/?client_id=63e5ba506c314fbe856734e0b8421191&redirect_uri=" +url+ "&response_type=token";
+  }
+  else {
+    var accessToken = url.substring(tokenIndex+14);
+  }
+  return accessToken;
 }
-else {
-  var accessToken = url.substring()
-}
-accessToken
