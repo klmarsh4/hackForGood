9481bbcd5adfc854d23f98bfea94778054eabcf2
Kevin Marsh <klmarsh4@gmail.com>
2016-03-05 20:32:42 -0500

    getText now gets sentiment using alchemy api

--- js/getText.js
+++ js/getText.js
@@ -1,8 +1,10 @@
 function getText(obj){
-  var caption = obj.data[0].caption.text;
-  var mediaId = obj.data[0].id;
+  var k = 4;
+  var caption = obj.data[k].caption.text;
+  var mediaId = obj.data[k].id;
   var accessToken = getAccessToken();
-  document.getElementById("p").innerHTML = caption
+  //document.getElementById("p").innerHTML = caption
+  var text = caption;
   $.ajax({
     method: "GET",
     url: "https://api.instagram.com/v1/media/"+mediaId+"/comments?access_token=" + accessToken,
@@ -11,13 +13,16 @@ function getText(obj){
     jsonpCallback: "jsonpcallback",
     success: function(obj){
       //var comments[];
-      console.log(obj);
+      //console.log(obj);
       for(var i=0; i<obj.data.length;i++){
         //comments.push(comment.text);
-        document.getElementById("p").innerHTML += ("<br>" + obj.data[i].text);
+        //document.getElementById("p").innerHTML += ("<br>" + obj.data[i].text);
+        text += (" " + obj.data[i].text);
       }
     },
     error: function(){console.log("error");},
-    complete: function(){console.log("complete");}
-});
+    complete: function(){$.post("http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment",
+    "apikey=9b6a97c25e1c0a3b5a2989591629d81aefabd14c&text="+text+"&outputMode=json",
+    function(obj){console.log(obj);});}
+  });
 }
