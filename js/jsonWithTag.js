dd07109ebee7bc1d0191d053e9ded3922bd20daa
Michael Vander Meiden <m.vandermeiden@umiami.edu>
2016-03-05 21:47:02 -0500

    Added jsonWithTag which takes a json obj and deletes everything in data array except the data with tag. Also changed serchTag to user input.

--- js/jsonWithTag.js
+++ js/jsonWithTag.js
@@ -22,25 +22,31 @@ function searchForTag(obj){
 	var dataCount=obj.data.length;
 	while (dataCount>0){
 		 var tagCount=obj.data[dataCount - 1].tags.length
+		 var tagMatch = false;
 		while (tagCount>0){
+			
+			//Looks to see if there is a matching tag, if not tagMatch stays false
+			
 			if (obj.data[dataCount-1].tags[tagCount-1] == tagName){
-
-				document.write(obj.data[dataCount-1].link + "<br>")
+				tagMatch = true;
 			}
-		tagCount=tagCount-1;
+			
+			tagCount=tagCount-1;
 		}
-		
+		//deletes data if there was no matching tag
+		if (tagMatch == false){
+			
+			obj.data.splice(dataCount-1,1);
+			dataCount = dataCount - 1;
+		}
+		else{
 		dataCount = dataCount - 1;
+		}
+	
 	}
-		
+	console.log(obj);
+	// CALL YOUR FUNCTION WITH NEW obj HERE
 	
 }
-
-/*
-function displayImg(obj){
-	//console.log(myJson);
-	//var obj = JSON.parse(myJson);
-	var imgUrl = obj.data[0].images.low_resolution.url;
-	window.location = imgUrl;
-}
-*/
+		
+	
